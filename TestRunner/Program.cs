f0d6dfffe379714ccb72c9a7203901e3c99cec2d
blitzcrank <lleeme20@student.aau.dk>
2024-04-26 09:44:24 +0200

    generalized testrunner

--- TestRunner/Program.cs
+++ TestRunner/Program.cs
@@ -1,35 +1,46 @@
 ﻿using System.Diagnostics;
 using System.Globalization;
 using System.Text.RegularExpressions;
+
 class Program
 {
-    public static void Main()
+    public const string PERFOPTIONS = "stat -e cache-references,cache-misses,\"power/energy-cores/\" -a";
+    public const string PATHTOOOP = "/home/blitzcrank/p6/oop/bin/Release/net8.0/oop";
+    public const string PATHTOREPLAYS = "/home/blitzcrank/p6/Replay/";
+    public const string REPLAYNAME = "game";
+    public static void Main(string[] args)
     {
-        // Process.Start("perf",  "stat record -e \"/power/energy-cores/\" -a /home/blitzcrank/p6/TestRunner/oop");
-        // Process.Start("perf",  "stat report");
-        Process perf = new Process();
-        ProcessStartInfo perfStartInfo = new ProcessStartInfo("perf");
-        perfStartInfo.Arguments = ("stat -e cache-references,cache-misses,\"power/energy-cores/\" -a /home/blitzcrank/p6/oop/bin/Release/net8.0/oop /home/blitzcrank/p6/Replay/game");
-        perfStartInfo.UseShellExecute = false;
-        perfStartInfo.RedirectStandardError = true;
-        perf.StartInfo = perfStartInfo;
-        List<MeasurementResults> results = new List<MeasurementResults>();
-        for(int i = 0; i<100; i++)
+        bool OOP = false;
+        bool DOP = false;
+        int iterations = 100;
+        if(args.Length == 0)
         {
-            perf.Start();
-            StreamReader reader = perf.StandardError;
-            string output = reader.ReadToEnd();
-            results.Add(new MeasurementResults(output));
+            Console.WriteLine("Must specify paradigm");
+        }
+        else
+        {
+            if(args[0] == "oop")
+            {
+                OOP = true;
+            }
+            if(args[0] == "dop")
+            {
+                DOP = true;
+            }
+        }
+        if(args.Length >= 2)
+        {
+            if(!int.TryParse(args[1],out iterations))
+            {
+                Console.WriteLine("Number of iterations must be an integer");
+            }
+        }
+        if(OOP)
+        {
+            TestRunner testRunner = new TestRunner(PERFOPTIONS, PATHTOOOP, PATHTOREPLAYS, REPLAYNAME);
+            testRunner.RunTest(iterations,"OOP");
         }
 
-        DateTime time = DateTime.Now;
-        StreamWriter writer = new StreamWriter("../Measurements/" + time.ToString("yyMMddH"));
-        writer.WriteLine("CacheRefs;CacheMisses;Energy(J);time(s)");
-        foreach(var measurement in results)
-        {
-            writer.WriteLine(measurement.ToString());
-        }
-        writer.Close();
     }
 }
 
@@ -55,5 +66,42 @@ class MeasurementResults
     public override string ToString()
     {
         return CacheRefs+";"+CacheMisses+";"+Energy+";"+ Seconds;
+    }
+}
+
+class TestRunner
+{
+    public TestRunner(string perfoptions, string pathtoexe, string pathtoreplays, string replay)
+    {
+        perf = new Process();
+        string arguments = $"{perfoptions} {pathtoexe} {pathtoreplays}{replay}";
+        perfStartInfo = new ProcessStartInfo("perf");
+        perfStartInfo.Arguments = arguments;
+        perfStartInfo.UseShellExecute = false;
+        perfStartInfo.RedirectStandardError = true;
+        perf.StartInfo = perfStartInfo;
+    }
+    Process perf;
+    ProcessStartInfo perfStartInfo;
+
+    public void RunTest(int iterations, string paradigm)
+    {
+        List<MeasurementResults> results = new List<MeasurementResults>();
+        for(int i = 0; i<iterations; i++)
+        {
+            perf.Start();
+            StreamReader reader = perf.StandardError;
+            string output = reader.ReadToEnd();
+            results.Add(new MeasurementResults(output));
+        }
+
+        DateTime time = DateTime.Now;
+        StreamWriter writer = new StreamWriter("../Measurements/"+paradigm+time.ToString("yyMMddHHmm"));
+        writer.WriteLine("CacheRefs;CacheMisses;Energy(J);time(s)");
+        foreach(var measurement in results)
+        {
+            writer.WriteLine(measurement.ToString());
+        }
+        writer.Close();
     }
 }
