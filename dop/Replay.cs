bf73e2f2dc09c2068fff6fba4831a44035318fd2
MBStarup <mavi.nexu@gmail.com>
2024-04-26 09:17:56 +0200

    copy pastaed recording and replaying to dop

---
+++ dop/Replay.cs
@@ -0,0 +1,99 @@
+namespace P6.DOP;
+
+public class Recording
+{
+    public Recording()
+    {
+        loopInputs = new List<LoopInputs>();
+    }
+    public StreamWriter writer;
+    public List<LoopInputs> loopInputs;
+    public void recordState(Game game)
+    {
+        loopInputs.Add(new LoopInputs(game.DeltaTime, game.KeyState));
+    }
+    public void saveRecording()
+    {
+        writer = new StreamWriter("game");
+        foreach (LoopInputs loop in loopInputs)
+        {
+            writer.WriteLine(loop.ToString());
+        }
+        writer.WriteLine("GAMEEND");
+        writer.Close();
+    }
+}
+public class LoopInputs
+{
+    public LoopInputs(float deltaTime, KeyState keystate)
+    {
+        DeltaTime = deltaTime;
+        KeyState = new KeyState
+        {
+            Up = keystate.Up,
+            Down = keystate.Down,
+            Left = keystate.Left,
+            Right = keystate.Right,
+            Shoot = keystate.Shoot
+        };
+
+    }
+    public LoopInputs(float deltaTime, string values)
+    {
+        DeltaTime = deltaTime;
+        KeyState = new KeyState
+        {
+            Up = uint.Parse(values[0].ToString()),
+            Down = uint.Parse(values[1].ToString()),
+            Left = uint.Parse(values[2].ToString()),
+            Right = uint.Parse(values[3].ToString()),
+            Shoot = int.Parse(values[4].ToString())
+        };
+    }
+    public float DeltaTime;
+    public KeyState KeyState;
+    private string keyStateString()
+    {
+        return KeyState.Up.ToString() + KeyState.Down.ToString() + KeyState.Left.ToString() + KeyState.Right.ToString() + KeyState.Shoot.ToString();
+    }
+    public override string ToString()
+    {
+        return DeltaTime.ToString() + " " + keyStateString();
+    }
+}
+
+public class Replay
+{
+    public Replay(string replayFile)
+    {
+        inputs = new List<LoopInputs>();
+        reader = new StreamReader(replayFile);
+        while ((line = reader.ReadLine()) != "GAMEEND")
+        {
+            splitline = line.Split(" ");
+            inputs.Add(new LoopInputs(float.Parse(splitline[0]), splitline[1]));
+        }
+    }
+    public List<LoopInputs> inputs;
+    private StreamReader reader;
+    private string[] splitline;
+    private string line;
+    private int replaystep = 0;
+    public void update(Game game)
+    {
+        if (replaystep >= inputs.Count)
+        {
+            game.KeyState.Close = 1;
+        }
+        else
+        {
+            game.DeltaTime = inputs[replaystep].DeltaTime;
+            game.KeyState.Up = inputs[replaystep].KeyState.Up;
+            game.KeyState.Down = inputs[replaystep].KeyState.Down;
+            game.KeyState.Left = inputs[replaystep].KeyState.Left;
+            game.KeyState.Right = inputs[replaystep].KeyState.Right;
+            game.KeyState.Shoot = inputs[replaystep].KeyState.Shoot;
+            replaystep++;
+        }
+    }
+}
