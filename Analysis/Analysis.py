25430a770040ed7478b53a3bde2e7b83188d69cd
Lauri <lleeme20@student.aau.dk>
2024-05-06 13:46:32 +0200

    More analysis

--- Analysis/Analysis.py
+++ Analysis/Analysis.py
@@ -1,3 +1,5 @@
+import math
+from scipy import stats as st
 import numpy as np
 import matplotlib.pyplot as plot
 
@@ -33,3 +35,58 @@ timedata = (OOPtime, DOPtime)
 plot.boxplot(timedata, labels=["OOP", "DOP"])
 plot.ylabel("Time (s)")
 plot.show()
+
+# OOPcacherefsSTD = np.std(OOPcacherefs)
+# OOPcacherefsVar = np.var(OOPcacherefs)
+# OOPcachemissesSTD = np.std(OOPcachemisses)
+# OOPcachemissesVar = np.var(OOPcachemisses)
+# OOPenergyuseSTD = np.std(OOPenergyuse)
+# OOPenergyuseVar = np.var(OOPenergyuse)
+
+# DOPcacherefsSTD = np.std(DOPcacherefs)
+# DOPcacherefsVar = np.var(DOPcacherefs)
+# DOPcachemissesSTD = np.std(DOPcachemisses)
+# DOPcachemissesVar = np.var(DOPcachemisses)
+# DOPenergyuseSTD = np.std(DOPenergyuse)
+# DOPenergyuseVar = np.var(DOPenergyuse)
+
+# def printStatistics(stddev, variance, name):
+#     print(name + " standard deviation: " + np.float64(stddev).astype(str) + "; variance: " +np.float64(variance).astype(str))
+
+# printStatistics(OOPcacherefsSTD, OOPcacherefsVar, "OOP cache references")
+# printStatistics(OOPcachemissesSTD, OOPcachemissesVar, "OOP cache misses")
+# printStatistics(OOPenergyuseSTD, OOPenergyuseVar, "OOP energy use")
+
+# printStatistics(DOPcacherefsSTD, DOPcacherefsVar, "DOP cache references")
+# printStatistics(DOPcachemissesSTD, DOPcachemissesVar, "DOP cache misses")
+# printStatistics(DOPenergyuseSTD, DOPenergyuseVar, "DOP energy use")
+
+def ConfidenceInterval(data, name, confidence = 0.95):
+    mean = np.mean(data)
+    sampleSize = len(data)
+    stddev = np.std(data)
+    interval = confidence*(stddev / math.sqrt(sampleSize))
+    print("Confidence interval for " +name+" is: " + np.float64(mean).astype(str) + "+-" + np.float64(interval).astype(str))    
+
+ConfidenceInterval(OOPcacherefs, "OOP cache references")
+ConfidenceInterval(OOPcachemisses, "OOP cache misses")
+ConfidenceInterval(OOPenergyuse, "OOP energy use")
+ConfidenceInterval(DOPcacherefs, "DOP cache references")
+ConfidenceInterval(DOPcachemisses, "DOP cache misses")
+ConfidenceInterval(DOPenergyuse, "DOP energy use")
+
+
+CacherefTTest = st.ttest_ind(OOPcacherefs, DOPcacherefs, equal_var=False)
+print("The P value for Cache refs is: " + np.float64(CacherefTTest.pvalue).astype(str))
+
+CachemissTTest = st.ttest_ind(OOPcachemisses, DOPcachemisses, equal_var=False)
+print("The P value for Cache misses is: " + np.float64(CachemissTTest.pvalue).astype(str))
+
+energyTTest = st.ttest_ind(OOPenergyuse, DOPenergyuse, equal_var=False)
+print("The P value for energy is: " + np.float64(energyTTest.pvalue).astype(str))
+
+OOPTimeEnergyR = st.pearsonr(OOPtime, OOPenergyuse)
+print("The R value for time and energy in OOP is: " + np.float64(OOPTimeEnergyR.statistic).astype(str))
+
+DOPTimeEnergyR = st.pearsonr(DOPtime, DOPenergyuse)
+print("The R value for time and energy in DOP is: " + np.float64(DOPTimeEnergyR.statistic).astype(str))
