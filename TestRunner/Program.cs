40c750cc7cf35090cb5026beebdb3c356dc4b26a
blitzcrank <lleeme20@student.aau.dk>
2024-05-15 15:40:56 +0200

    updated test runner to read more things

--- TestRunner/Program.cs
+++ TestRunner/Program.cs
@@ -1,10 +1,28 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Text;
+using System.Diagnostics;
 using System.Globalization;
 using System.Text.RegularExpressions;
 
 class Program
 {
-    public const string PERFOPTIONS = "stat -e cache-references,cache-misses,\"power/energy-cores/\" -a";
+    public static string ParseInt(string str)  {System.Console.WriteLine($"parsing int: {str}"); return Int64.Parse(str, NumberStyles.AllowThousands).ToString();}
+    public static string ParseDouble(string str) => double.Parse(str, NumberStyles.AllowThousands | NumberStyles.AllowDecimalPoint).ToString();
+    public static (string Name, Func<string, string> Parser)[] PERFSTATS = [
+        ("cache-references",            ParseInt),
+        ("cache-misses",                ParseInt),
+        ("L1-dcache-loads",             ParseInt),
+        ("L1-dcache-load-misses",       ParseInt),
+        ("L1-dcache-stores",            ParseInt),
+        ("L1-icache-load-misses",       ParseInt),
+        ("LLC-loads",                   ParseInt),
+        ("LLC-load-misses",             ParseInt),
+        ("LLC-stores",                  ParseInt),
+        ("LLC-store-misses",            ParseInt),
+        ("power/energy-cores/",         ParseDouble)
+    ];
+
+    public static string PERFOPTIONS = $"stat -e \"{String.Join("\",\"",PERFSTATS.Select(x => x.Name))}\" -a";
     public static List<(string Path, string Name)> Games = [];
     public static Queue<string> Replays = [];
     public static string USAGE = $"Usage: {System.AppDomain.CurrentDomain.FriendlyName} iterations {{-p game [-n name]}}... replay...";
@@ -16,7 +34,7 @@ class Program
     }
     private static void ExitWithUsage() => ExitWithError(USAGE);
     public static void Main(string[] args)
-    {
+    {    
         if (args.Length < 4) ExitWithUsage();
 
         var argQueue = new Queue<string>(args);
@@ -106,21 +124,27 @@ class TestRunner
         System.Console.Write($"Running test with replay {replay} on game {name}: ");
         var (cursor_left, cursor_top) = Console.GetCursorPosition();
         var outfile = Path.Combine("..", "Measurements", $"{replay}_{name}_{DateTime.Now.ToString("yyMMddHHmm")}_{iterations}.data");
-        List<MeasurementResults> results = new List<MeasurementResults>();
+        List<string> results = new List<string>(iterations);
         for (int i = 0; i < iterations; i++)
         {
+            StringBuilder result = new();
             Console.SetCursorPosition(cursor_left, cursor_top);
             System.Console.Write(i + 1);
             perf.Start();
             StreamReader reader = perf.StandardError;
-            string output = reader.ReadToEnd();
-            results.Add(new MeasurementResults(output));
+            System.Console.WriteLine("--------------");
+            for (int j = 0; j < 3; j++)reader.ReadLine(); //skip empty/usless lines
+            for (int j = 0; j < Program.PERFSTATS.Count(); j++) {result.Append(Program.PERFSTATS[j].Parser(reader.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)[0])); result.Append(";");}
+            for (int j = 0; j < 1; j++)reader.ReadLine(); //skip empty line
+            for (int j = 0; j < 1; j++) result.Append(Program.ParseDouble(reader.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)[0])); //read time
+
+            results.Add(result.ToString());
         }
         System.Console.WriteLine($"\nDone, writing to file {outfile}\n");
 
         StreamWriter writer = new StreamWriter(outfile);
-        writer.WriteLine("CacheRefs;CacheMisses;Energy(J);time(s)");
-        foreach (var measurement in results) writer.WriteLine(measurement.ToString());
+        writer.WriteLine($"{String.Join(";",Program.PERFSTATS.Select(x => x.Name))};time");
+        foreach (var result in results) writer.WriteLine(result.ToString());
         writer.Close();
     }
 }
